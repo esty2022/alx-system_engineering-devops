7bd9b9fd3d134ba19561a265dc07bdafa8df7491
esty2022 <esther.duntoye@gmail.com>
2023-06-30 02:55:01 -0700

    transfe file from client to server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -14,7 +14,7 @@ exec {'install':
 exec {'echo_html':
   provider => shell,
   path     => '/usr/bin:/usr/sbin:/bin',
-  command  => 'sudo echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
+  command  => 'sudo echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html',
 }
 
 exec {'sed_config':
