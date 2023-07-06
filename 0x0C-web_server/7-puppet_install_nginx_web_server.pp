a4742cb3fdebe1c419b0f05846adf6da92b21e7c
esty2022 <esther.duntoye@gmail.com>
2023-07-06 01:15:34 -0700

    add

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -14,11 +14,11 @@ exec {'install':
 exec {'echo_html':
   provider => shell,
   path     => '/usr/bin:/usr/sbin:/bin',
-  command  => 'sudo echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html',
+  command  => 'sudo echo "Hello World!" | sudo tee /var/www/html/index.html',
 }
 
 exec {'sed_config':
-  command  => 'sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.youtube.com permanent;" /etc/nginx/sites-available/default',
+  command  => 'sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.github.com/esty2022 permanent;" /etc/nginx/sites-enabled/default',
   provider => shell,
   path     => '/usr/bin:/usr/sbin:/bin',
 }
